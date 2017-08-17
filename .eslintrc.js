68cf5516c136975804baaf13fdae407879be4930
Thunf <458859360@qq.com>
2017-08-17 17:42:47 +0800

    upd: 增加wx全局对象eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,8 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'space-before-function-paren': 0
+  },
+  globals: {
+    wx: true
   }
 }
