0e137abf65e4b3b1cd33956d7666dcd506c13519
Thunf <458859360@qq.com>
2017-09-01 19:29:28 +0800

    add: 抽出示例图片数据到demo.js

---
+++ src/mixins/demo.js
@@ -0,0 +1,15 @@
+// 示例放置图片组
+const funImages = [
+  '/images/category/cate-da@2x.png',
+  '/images/category/cate-ji@2x.png',
+  '/images/category/cate-da@2x.png',
+  '/images/category/cate-li@2x.png',
+  '/images/category/cate-jin@2x.png',
+  '/images/category/cate-wan@2x.png',
+  '/images/category/cate-chi@2x.png',
+  '/images/category/cate-jii@2x.png'
+]
+
+export {
+  funImages
+}
