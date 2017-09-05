ea5c6fa4419f2532a9f4e33d2ac5d8b28db32f92
Thunf <458859360@qq.com>
2017-09-05 21:10:16 +0800

    add: 封装统一弹框提示

--- src/mixins/base.js
+++ src/mixins/base.js
@@ -96,4 +96,22 @@ export default class baseMixin extends wepy.mixin {
   isPhone(str) {
     return /^1\d{10}$/.test(str)
   }
+  $alert(item = '标题', item2) {
+    const param = this.isObject(item) ? Object.assign({
+      // 首参数为obj
+      title: 'title', content: 'content'
+    }, item) : this.isString(item) ? this.isString(item2) ? {
+      // 俩参数均为字符串
+      title: item, content: item2
+    } : {
+      // 只有首参为字符串
+      title: '', content: item
+    } : {
+      // 尝试转换字符串
+      title: item.toString ? item.toString() : '参数异常'
+    }
+    wx.showModal(Object.assign({
+      showCancel: false
+    }, param))
+  }
 }
