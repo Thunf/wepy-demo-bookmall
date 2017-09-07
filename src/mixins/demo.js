b1a0870d07c03a4a5f1d73823ad6c8735e85ce80
Thunf <458859360@qq.com>
2017-09-07 20:21:51 +0800

    add: 添加示例套餐组数据

--- src/mixins/demo.js
+++ src/mixins/demo.js
@@ -1,4 +1,4 @@
-// 示例放置图片组
+// 示例:图片组
 const funImages = [
   '/images/category/cate-da@2x.png',
   '/images/category/cate-ji@2x.png',
@@ -10,6 +10,22 @@ const funImages = [
   '/images/category/cate-jii@2x.png'
 ]
 
+// 示例:套餐组
+const packages = new Array(5).fill(0).map((x, i) => {
+  const index = `${i + 1}`
+  const pow = `${Math.pow(index, 2)}`
+  const qtt = `${Math.floor(Math.pow(index, 1.5))}`
+  const pri = `${8.88 + Math.floor(Math.pow(i, 5))}`
+  return {
+    id: index,
+    period: pow,
+    times: index,
+    quantity: qtt,
+    price: pri
+  }
+})
+
 export {
-  funImages
+  funImages,
+  packages
 }
